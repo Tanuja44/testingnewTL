d90adf2ffa6565910a3dbd63a7fcd5c71524087a
dev2 <dev2@gmail.com>
2020-10-18 15:20:32 +0530

    dev1 made changes

--- DemoProj1/src/com/nt/bin/utility.java
+++ DemoProj1/src/com/nt/bin/utility.java
@@ -4,6 +4,11 @@ import java.util.Date;
 
 public class Utility {
 	
-public Date getSysDate()
+public Date getSysDate() {
 return new Date();
+}
+
+public void m1() {
+	System.out.println(Utility.m1());
+}
 }
