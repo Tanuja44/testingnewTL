3b9adb6018a5231b9d4168fc0cc7bdc74576db4e
dev2 <dev2@gmail.com>
2020-10-18 15:31:47 +0530

    conflicts solved

--- DemoProj1/src/com/nt/bin/utility.java
+++ DemoProj1/src/com/nt/bin/utility.java
@@ -7,11 +7,3 @@ public class Utility {
 public Date getSysDate() {
 return new Date();
 }
-
-<<<<<<< HEAD
-public void m1() {
-	System.out.println(Utility.m1());
-}
-}
-=======
->>>>>>> branch 'master' of https://github.com/Tanuja44/testingnewTL
